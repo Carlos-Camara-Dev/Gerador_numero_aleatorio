ec9226b728c8054bdf82de48e9ee1d1373b512ec
Carlos-Camara-Dev <carloscamara.dev@gmail.com>
2024-06-16 22:18:34 -0300

    Projeto estilizado

--- index.php
+++ index.php
@@ -1,4 +1,7 @@
 <?php
+// if ($_GET["action"]) {
+//     session_start();
+// } session_start();
 session_start();
 ?>
 <!DOCTYPE html>
@@ -8,33 +11,35 @@ session_start();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="overall_style.css">
 </head>
 
 <body>
-    <main>
+    <main class="main">
         <section>
-            <h2>Número inteiro aleátorio</h2>
-            <form action="random_number.php?action=int" method="post">
+
+            <form action="random_number.php?action=int" method="post" id="form">
+                <h2>Número inteiro aleátorio</h2>
                 <label for="min"> Valor minimo: </label>
                 <input type="text" id="min" name="min" require>
                 <br>
                 <label for="max"> Valor maximo: </label>
                 <input type="text" id="max" name="max" require>
                 <br>
-                <button name="verify">Draw lots</button>
+                <button name="verify" id="buttom">Draw lots</button>
             </form>
-            <article>
-                <?php
-                if ($_SESSION['tipo'] == "int") {
-                    include_once("result_random.php");
-                }
-                ?>
-            </article>
+
         </section>
+        <article class="result">
+            <?php
+            if ($_SESSION['tipo'] == "int") {
+                include_once("result_random.php");
+            }
+            ?>
+        </article>
         <section>
-            <h2>Número com virgula aleátorio
-            </h2>
-            <form action="random_number.php?action=float" method="post">
+            <form action="random_number.php?action=float" method="post" id="form">
+                <h2>Número com virgula aleátorio</h2>
                 <label for="min"> Valor minimo: </label>
                 <input type="text" id="min" name="min" require>
                 <br>
@@ -44,16 +49,17 @@ session_start();
                 <label for="house"> Valor de casas decimais: </label>
                 <input type="text" id="house" name="house" require>
                 <br>
-                <button name="verify">Draw lots</button>
+                <button name="verify" id="buttom">Draw lots</button>
             </form>
-            <article>
-                <?php
-                if ($_SESSION['tipo'] == "float") {
-                    include_once("result_random.php");
-                }
-                ?>
-            </article>
+
         </section>
+        <article class="result">
+            <?php
+            if ($_SESSION['tipo'] == "float") {
+                include_once("result_random.php");
+            }
+            ?>
+        </article>
     </main>
 </body>
 
